131d064663d805221fdb88a4f23145f5b8217be4
Gregg Kellogg <gregg@kellogg-assoc.com>
2010-06-03 16:03:21 -0700

    * Added array_hacks, nokogiri_hacks, and rdf_escape

---
+++ lib/rdf/rdfxml/patches/nokogiri_hacks.rb
@@ -0,0 +1,8 @@
+require 'nokogiri'
+class Nokogiri::XML::Node
+  # URI of namespace + node_name
+  def uri
+    ns = self.namespace ? self.namespace.href : RdfContext::XML_NS.uri.to_s
+    RdfContext::URIRef.new(ns + self.node_name)
+  end
+end unless Nokogiri::XML::Node.method_defined?(:uri)
