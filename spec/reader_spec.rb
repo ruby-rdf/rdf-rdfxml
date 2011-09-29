96d342cdf2dc223ff7c328d780697c36db636c0a
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-09-29 11:41:01 -0700

    Move rdfcore tests from reader_spec to rdfcore_spec

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -329,59 +329,6 @@ EOF
     end
   end
 
-  # W3C Test suite from http://www.w3.org/2000/10/rdf-tests/rdfcore/
-  describe "w3c rdfcore tests" do
-    require 'rdf_test'
-    
-    # Positive parser tests should raise errors.
-    describe "positive parser tests" do
-      Fixtures::TestCase::PositiveParserTest.each do |t|
-        next unless t.status == "APPROVED"
-        #next unless t.about =~ /rdfms-rdf-names-use/
-        #next unless t.name =~ /11/
-        #puts t.inspect
-        specify "#{t.name}: " + (t.description || "#{t.inputDocument} against #{t.outputDocument}") do
-          begin
-            graph = RDF::Graph.new << RDF::RDFXML::Reader.new(t.input,
-              :base_uri => t.inputDocument,
-              :validate => false,
-              :debug => t.debug)
-
-            # Parse result graph
-            #puts "parse #{self.outputDocument} as #{RDF::Reader.for(self.outputDocument)}"
-            format = detect_format(t.output)
-            output_graph = RDF::Graph.load(t.outputDocument, :format => format, :base_uri => t.inputDocument)
-            puts "result: #{CGI.escapeHTML(graph.dump(:ntriples))}" if ::RDF::N3::debug?
-            graph.should be_equivalent_graph(output_graph, t)
-          rescue RSpec::Expectations::ExpectationNotMetError => e
-            if t.inputDocument =~ %r(xml-literal|xml-canon)
-              pending("XMLLiteral canonicalization not implemented yet")
-            else
-              raise
-            end
-          end
-        end
-      end
-    end
-    
-    # Negative parser tests should raise errors.
-    describe "negative parser tests" do
-      Fixtures::TestCase::NegativeParserTest.each do |t|
-        next unless t.status == "APPROVED"
-        #next unless t.about =~ /rdfms-empty-property-elements/
-        #next unless t.name =~ /1/
-        #puts t.inspect
-        specify "test #{t.name}: " + (t.description || t.inputDocument) do
-          lambda do
-            RDF::Graph.new << RDF::RDFXML::Reader.new(t.input,
-              :base_uri => t.inputDocument,
-              :validate => true)
-          end.should raise_error(RDF::ReaderError)
-        end
-      end
-    end
-  end
-  
   def parse(input, options)
     @debug = []
     graph = RDF::Graph.new
