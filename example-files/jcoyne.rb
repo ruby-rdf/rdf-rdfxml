42c082952470dba22cf07b562d77d0e94ea5b2c3
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-12-05 21:53:10 -0800

    Add Justin Coyne's RDF/XML serialization issue where unnecessary nodeID attributes are rendered.

---
+++ example-files/jcoyne.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "..", 'lib')))
+require "bundler/setup"
+require 'rdf/rdfxml'
+
+class Foo < RDF::Vocabulary('http://example.com#')
+  property :hasList
+end
+
+g = RDF::Graph.new
+u = RDF::URI.new('info:fedora/999')
+
+list1 = RDF::Node.new
+list2 = RDF::Node.new
+
+leaf1 = RDF::Node.new
+leaf2 = RDF::Node.new
+
+g.insert([leaf1, RDF::DC.title, 'Hi'   ])
+g.insert([leaf2, RDF::DC.title, 'There'])
+g.insert([list1,  RDF.first,     leaf1  ])
+g.insert([list1,  RDF.rest,      list2  ])
+g.insert([list2,  RDF.first,     leaf2  ])
+g.insert([list2,  RDF.rest,      RDF.nil])
+g.insert([u,     Foo.hasList,   list1   ])
+
+puts g.dump(:rdfxml)
+puts g.dump(:ntriples)
